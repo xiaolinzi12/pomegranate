64bb7f9f0b438a068b0dd63cf4c8115ef4f3d0dd
zhenglin <zhengl@quantum1tech.com>
2019-08-23 21:22:03 +0800

    init

---
+++ src/main/java/top/ok/fine/pomegranate/PomegranateApplication.java
@@ -0,0 +1,13 @@
+package top.ok.fine.pomegranate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomegranateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomegranateApplication.class, args);
+	}
+
+}
